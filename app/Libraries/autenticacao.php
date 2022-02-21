9f123fb9c9996d9d1b815242f8ae2246d1d653a6
ALBERTO CESAR <86452527+betinhobets2@users.noreply.github.com>
2022-02-21 14:39:13 -0300

    Aula 57 do curso de Codeigniter 4

--- app/Libraries/autenticacao.php
+++ app/Libraries/autenticacao.php
@@ -34,8 +34,55 @@ class autenticacao {
         // Nesse ponto esta tudo Certo e podemos logar o usuario na aplicação invocando o método abaixo
         $this->logaUsuario($usuario);
 
+        //retornando True.... tudo Certo!
         return true;
          
+    }
+
+    public function logout() {
+
+        session()->destroy();
+    }
+
+
+    public function pegaUsuarioLogado() {
+
+        /**
+         * Não esquecer de compartilhar a instância com services
+         */
+        if($this->usuario === null) {
+
+            $this->usuario = $this->pegaUsuarioDaSessao();
+
+        }
+        /**
+         * retornamos o usuaário que foi definido no inicio da classe
+         */
+        return $this->usuario;
+    }
+
+    public function estaLogado () {
+
+        return $this->pegaUsuarioLogado() !== null;
+    }
+
+    private function pegaUsuarioDaSessao() {
+
+        if(!session()->has('usuario_id')) {
+
+            return null;
+        }
+        // instanciamos o model usuario
+        $usuarioModel = new App\Models\UsuarioModel();
+
+        //recurpero o usuario de acordo com a sessão 'usuario'_id'
+        $usuario = $usuarioModel->find(session()->get('usuario_id')); 
+        
+        //só retorno o objeto $usuario se o mesmo for encontro e estiver ativo
+        if($usuario && $usuario->ativo){
+
+            return $usuario;
+        }
     }
 
     private function logaUsuario(object $usuario){
