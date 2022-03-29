5ed3156de507cd675c68986f880633a175169dc7
ALBERTO CESAR <86452527+betinhobets2@users.noreply.github.com>
2022-03-29 16:34:58 -0300

    Foi criado as formas de pagamento e as Seeders

---
+++ app/Controllers/Admin/FormasPagamentos.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controllers\Admin;
+
+use App\Controllers\BaseController;
+
+class FormasPagamentos extends BaseController
+{
+    public function index()
+    {
+        //
+    }
+}
